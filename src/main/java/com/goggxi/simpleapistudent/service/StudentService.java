90288687c95f3fc1a5c2968a99672b1af7f4016e
goggxi <goggxi@gmail.com>
2021-03-04 02:11:04 +0800

    DELETE Method REST student

--- src/main/java/com/goggxi/simpleapistudent/service/StudentService.java
+++ src/main/java/com/goggxi/simpleapistudent/service/StudentService.java
@@ -32,4 +32,13 @@ public class StudentService {
 
         studentRepository.save(student);
     }
+
+    public void deleteStudent(Long studentId) {
+        boolean exist = studentRepository.existsById(studentId);
+        if (!exist){
+            throw new IllegalStateException("Student with id " +studentId+ " does not exist");
+        }
+
+        studentRepository.deleteById(studentId);
+    }
 }
